9e0d4ddd2840e8ffb059b6d18a69f00e2809db11
Guillermo Valle <guillefix@gmail.com>
2022-08-04 20:48:32 +0200

    some error logs

--- metagen/Interactions/MetaInteraction.cs
+++ metagen/Interactions/MetaInteraction.cs
@@ -14,12 +14,14 @@ namespace metagen.Interactions
         private MetaGen metagen_comp;
         public VoiceInteraction voiceInteraction;
         public TextInteraction textInteraction;
+        public FieldInteraction fieldInteraction;
         public PoseStreamInteraction poseStreamInteraction;
         public MetaInteraction(MetaGen component)
         {
             metagen_comp = component;
             voiceInteraction = new VoiceInteraction(metagen_comp);
             textInteraction = new TextInteraction(metagen_comp);
+            fieldInteraction = new FieldInteraction(metagen_comp);
             poseStreamInteraction = new PoseStreamInteraction(metagen_comp);
         }
 
@@ -34,17 +36,41 @@ namespace metagen.Interactions
 
         public void StartInteracting()
         {
-            if (!poseStreamInteraction.isInteracting)
-            {
-                poseStreamInteraction.StartInteracting();
+            try {
+                if (!poseStreamInteraction.isInteracting)
+                {
+                    poseStreamInteraction.StartInteracting();
+                }
+            } catch (Exception e) {
+                   UniLog.Log("OwO error in bakeAsync: " + e.Message);
+                   UniLog.Log(e.StackTrace);
             }
-            if (!voiceInteraction.isInteracting)
-            {
-                voiceInteraction.StartInteracting();
+            try {
+                if (!voiceInteraction.isInteracting)
+                {
+                    voiceInteraction.StartInteracting();
+                }
+            } catch (Exception e) {
+                   UniLog.Log("OwO error in bakeAsync: " + e.Message);
+                   UniLog.Log(e.StackTrace);
             }
-            if (!textInteraction.isInteracting)
-            {
-                textInteraction.StartInteracting();
+            try {
+                if (!textInteraction.isInteracting)
+                {
+                    textInteraction.StartInteracting();
+                }
+            } catch (Exception e) {
+                    UniLog.Log("OwO error in bakeAsync: " + e.Message);
+                    UniLog.Log(e.StackTrace);
+            }
+            try {
+                if (!fieldInteraction.isInteracting)
+                {
+                    fieldInteraction.StartInteracting();
+                }
+            } catch (Exception e) {
+                    UniLog.Log("OwO error in bakeAsync: " + e.Message);
+                    UniLog.Log(e.StackTrace);
             }
             isInteracting = true;
         }
@@ -61,6 +87,10 @@ namespace metagen.Interactions
             if (textInteraction.isInteracting)
             {
                 textInteraction.StopInteracting();
+            }
+            if (fieldInteraction.isInteracting)
+            {
+                fieldInteraction.StopInteracting();
             }
             isInteracting = false;
         }
